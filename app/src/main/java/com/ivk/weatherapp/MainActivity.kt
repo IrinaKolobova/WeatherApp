7c9a1681d912838f6f2035bf0cf45315e879f7dc
Irina Kolobova <kolobovairinav@gmail.com>
2020-10-07 14:54:40 -0700

    Create base UI

--- app/src/main/java/com/ivk/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ivk/weatherapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.ivk.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.content_main.*
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
     }
 }
