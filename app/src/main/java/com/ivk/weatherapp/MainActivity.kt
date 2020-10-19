fd9745e96286f30cf66c01227d4dda9fb29cf514
Irina Kolobova <kolobovairinav@gmail.com>
2020-10-19 06:34:18 -0700

    Fix location and swipe to update

--- app/src/main/java/com/ivk/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ivk/weatherapp/MainActivity.kt
@@ -2,25 +2,34 @@ package com.ivk.weatherapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
 import android.location.Geocoder
 import android.location.Location
+import android.location.LocationManager
 import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.gms.location.*
-import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.util.*
 import kotlin.collections.ArrayList
 
+//TODO: clean up
+//TODO: show city in toolbar
+//TODO: add option to select city and units
+//TODO: add settings
 
 private const val TAG = "MainActivity"
 private val weatherRVAdapter = WeatherRVAdapter(ArrayList())
@@ -30,18 +39,23 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
     RecyclerItemClickListener.OnRecyclerClickListener {
 
     private val OPEN_WEATHER_MAP_KEY: String = "b9331c3f8b9f662176fbd39baabf3f9a"
-    private val OPEN_WEATHER_MAP_BASE_URL: String =
-        "https://api.openweathermap.org/data/2.5/onecall"
+    private val OPEN_WEATHER_MAP_BASE_URL: String = "https://api.openweathermap.org/data/2.5/onecall"
     private val units: String = "imperial"
     private var latitude: String = "0.0"
     private var longitude: String = "0.0"
     private val requestPermissionCode = 1
     private var mLocation: Location? = null
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    //private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var cityName: String = " "
     private var stateName: String = " "
     private var countryName: String = " "
-    private lateinit var swipeLayout: SwipeRefreshLayout
+    private lateinit var swipeLayout: SwipeRefreshLayout //by Delegates.lazy { findViewById(R.id.swipeContainer) as SwipeRefreshLayout }
+    //lateinit var locationRequest: LocationRequest
+
+    // 4.0 version of location
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    val PERMISSION_ID = 1010
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate starts")
@@ -52,23 +66,149 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
 
         setRecyclerView()
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        getLastLocation()
-        Log.d(TAG, "onCreate: latitude = $latitude, LONGITUDE = $longitude")
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        getLocation()
 
-        Log.d(
-            TAG,
-            "onCreate: cityName = $cityName, stateName = $stateName, countryName = $countryName"
-        )
+        Log.d(TAG, "onCreate: latitude = $latitude, LONGITUDE = $longitude")
+        Log.d(TAG, "onCreate: cityName = $cityName, stateName = $stateName, countryName = $countryName")
+
+        requestAPI()
+
+        //set city name in toolbar
         toolbar.title = cityName
 
         swipeLayout = findViewById(R.id.swipeContainer)
-        swipeLayout.setOnRefreshListener( {
+        swipeLayout.setOnRefreshListener({
             val intent = intent
             finish()
             startActivity(intent)
+            //swipeLayout.isRefreshing = false
+
+            //this.recreate()                 // refresh your list contents somehow
             swipeLayout.isRefreshing = false
+            /*// To keep animation for 4 seconds
+            Handler().postDelayed(Runnable { // Stop animation (This will be after 3 seconds)
+                swipeLayout.setRefreshing(true)
+            }, 4000) // Delay in millis*/
         })
+        Log.d(TAG, "onCreate finished")
+    }
+
+    private fun getLocation() {
+        // check location permission
+        Log.d(TAG, "getLastLocation: calling checkPermission()")
+        if (checkPermission()) {
+            Log.d(TAG, "getLastLocation: location permission is granted")
+            // check if location service is enabled
+            if (isLocationEnabled()) {
+                Log.d(TAG, "getLastLocation: location service is enabled")
+                // get the location
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
+                    val location: Location? = task.result
+
+                    if (location == null) {
+                        // if location is null get the new user's location
+                        Log.d(TAG, "getLastLocation: location is null, getting new user's location")
+                        getNewLocation()
+                        Log.d(TAG, "getLastLocation: before getNewLocation() latitude = $latitude, longitude = $longitude")
+                        latitude = location?.latitude.toString()
+                        longitude = location?.longitude.toString()
+                        Log.d(TAG, "getLastLocation: after getNewLocation() latitude = $latitude, longitude = $longitude")
+                        requestAPI()
+                    } else {
+                        // set latitude and longitude
+                        Log.d(TAG, "getLastLocation: latitude = $latitude, longitude = $longitude")
+                        latitude = location.latitude.toString()
+                        longitude = location.longitude.toString()
+                        Log.d(TAG, "getLastLocation: latitude = $latitude, longitude = $longitude")
+                        requestAPI()
+                    }
+                }
+            } else {
+                //ask user to enable location services
+                Log.d(TAG, "getLastLocation: asking user to enable location services")
+                //TODO: change to snackbar
+                Toast.makeText(this, "Please enable your location", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            // ask for location permission
+            Log.d(TAG, "getLastLocation: asking user for location permission")
+            requestPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getNewLocation() {
+        Log.d(TAG, "getNewLocation: started")
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 0
+        locationRequest.fastestInterval = 0
+        locationRequest.numUpdates = 2 // (?) 1
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
+        // TODO: find the way to update location here
+        Log.d(TAG, "getNewLocation: finished")
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            Log.d(TAG, "locationCallback: onLocationResult started")
+            val lastLocation = p0.lastLocation
+            Log.d(TAG, "locationCallback: latitude = $latitude, longitude = $longitude")
+            // set new location
+            latitude = lastLocation.latitude.toString()
+            longitude = lastLocation.longitude.toString()
+            Log.d(TAG, "getLastLocation: new location were set \nlatitude = $latitude, longitude = $longitude")
+            requestAPI()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        Log.d(TAG, "checkPermission: checking location permission")
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        Log.d(TAG, "requestPermission: requesting location permission")
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            PERMISSION_ID
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "onRequestPermissionsResult: location permission granted")
+                Log.d(TAG, "onRequestPermissionsResult: getting new location via getNewLocation()")
+                getNewLocation()
+            }
+        }
     }
 
     private fun setRecyclerView() {
@@ -89,104 +229,9 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
         getRawData.execute(openWeatherUrl)
     }
 
-    @SuppressLint("MissingPermission")
-    private fun getLastLocation() {
-        Log.d(TAG, "getLastLocation: starts")
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            Log.d(TAG, "getLastLocation: calling requestPermission()1")
-            requestPermission()
-        } else {
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    Log.d(TAG, "getLastLocation: callback")
-                    mLocation = location
-                    if (location != null) {
-                        latitude = location.latitude.toString()
-                        longitude = location.longitude.toString()
-
-                        Log.d(TAG, "getLastLocation: latitude = $latitude, longitude = $longitude")
-                        requestAPI()
-                    } else {
-                        Log.d(TAG, "getLastLocation: calling requestNewLocationData()1")
-                        requestNewLocationData()
-                    }
-                }
-            Log.d(TAG, "getLastLocation: callback completed")
-
-        }
-        Log.d(TAG, "getLastLocation: calling requestNewLocationData()2")
-        requestNewLocationData()
-        Log.d(TAG, "getLastLocation: completed")
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun requestNewLocationData() {
-        Log.d(TAG, "requestNewLocationData: starts")
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermission()
-        }
-        Log.d(TAG, "requestNewLocationData: permission granted")
-        val locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 1
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest, locationCallback,
-            Looper.myLooper()
-        )
-
-        Log.d(TAG, "requestNewLocationData: latitude = $latitude, longitude = $longitude")
-        requestAPI()
-
-        Log.d(TAG, "requestNewLocationData: finished")
-    }
-
-
-    private val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            val lastLocation: Location = locationResult.lastLocation
-            Log.d(TAG, "locationCallback: ${lastLocation.longitude}")
-            toolbar.title =
-                "You Last Location is : Long: ${lastLocation.longitude} , Lat: ${lastLocation.latitude}"
-            cityName = getCityName(lastLocation.latitude, lastLocation.longitude)
-        }
-    }
-
-
-    private fun requestPermission() {
-        Log.d(TAG, "requestPermission: started")
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-            requestPermissionCode
-        )
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (requestCode == requestPermissionCode) {
-            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                getLastLocation()
-            }
-        }
-    }
-
     override fun onItemClick(view: View, position: Int) {
         Log.d(TAG, "onItemClick: normal tap at position $position")
+        //val weatherData = weatherRVAdapter.getWeatherData(position)
     }
 
     override fun onItemLongClick(view: View, position: Int) {
@@ -225,6 +270,8 @@ class MainActivity : AppCompatActivity(), GetRawData.OnDownloadComplete,
 
 
     private fun getCityName(lat: Double, long: Double): String {
+        //var cityName:String = ""
+        //var countryName = ""
         val geoCoder = Geocoder(this, Locale.getDefault())
         val address = geoCoder.getFromLocation(lat, long, 3)
 
